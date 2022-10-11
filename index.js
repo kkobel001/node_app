759cf7e11980464d15eb8be02699667a5ce2abf2
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-10-11 19:58:28 +0200

    prepare plug to project

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log('hello word');
 
-4+4;
+const add = require('./math');
+const sayHi =(name='') => {
+
+    const args = process.argv.splice(2)
+    for( const a of args){
+       const arr = a.split('=');
+       if (arr[0]=== '--name') {
+        name=arr[1];
+    }
+    }
+   
+    console.log(`Cześc ${name}`);
+
+}
+
+
+sayHi();
+
+
+
+
