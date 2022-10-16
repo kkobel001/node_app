916f710199a808f894793d9d387c0539f083a321
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-10-16 22:40:21 +0200

    Create server

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-const {add} = require('./math-module');
+const http= require('http');
+
+function requestListener(req,res) {
+    console.log('Ktos wszedl na strone');
+    console.log(req.url);
+
+    if (req.url === "/kontakt"){
+        res.write('<h1>to jest kontakt</h1>')
+        res.end()
+    }
+    if (req.url === "/"){
+        res.write('<h1>to jest strona główna</h1>')
+        res.end()
+    }
+    res.write('<h1>404</h1>')
+     return res.end()
 
 
-console.log(add(2,5));
-console.log(sub(10,5));
 
+    // res.end('Hello node');
+}
 
+const serwer = http.createServer(requestListener);
 
-
+serwer.listen(3000,()=>{
+    console.log('Serwer slucha na porcie https:localhost:3000')
+});
