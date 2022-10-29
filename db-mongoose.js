4ffecf18dd6c53e5445be621afc5220b2755d52e
Katarzyna Kobel <katarzyna.kobel@vp.pl>
2022-10-29 20:24:58 +0200

    Add mongoose

--- db-mongoose.js
+++ db-mongoose.js
@@ -4,15 +4,35 @@ const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost:27017/node-project');
 
-const Company = new Schema({
+const chechForbidenString = (value, forbidenString) => {
+        if(value === forbidenString ) {
+            throw new Error(`Nazwa ${forbidenString} jest zakazana`);
+        }
+}
+
+const CompanySchema = new Schema({
     slug: {
         type: String,
+        required: [true, 'Pole slug jest wymagane'],
+        minLength:[3, 'Minimalna liczba znaków ot 3'],
+        validate : value =>chechForbidenString(value, 'slug'),
+        trim: true,
+        // lowercase: true, 
     },
     name: {
         type:String,
+        required: [true, 'Pole name jest wymagane'],
+
+    },
+    empoyesCount :{
+        type:Number,
+        min:1 
     }
 });
-const MyModel = mongoose.model('Company', Company);
+CompanySchema.path('slug').set((value) => value.toLowerCase());
+
+
+const MyModel = mongoose.model('Company', CompanySchema);
 
 async function findcompany() {
     const companies = await MyModel.find({});
@@ -24,10 +44,20 @@ findcompany();
 
 async function newCompany() {
     const company = new MyModel({
-        name:'test',
-        slug:'test'
+        name:'kkk',
+        slug:'       kkkKKK',
     })
-    await company.save();
+    try {
+        await company.save();
+
+    } catch (e) {
+        console.log('Cos poszło nie tak...');
+        for (const key in e.errors){
+            console.log(e.errors[key].message);
+
+
+        }
+    }
 }
 
 newCompany();
